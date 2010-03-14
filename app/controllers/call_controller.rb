03aa81dcc164340200de91e937fae6c6e81dab74
Jaymes Waters <Jaym3s@gmail.com>
2010-03-13 18:49:10 -0600

    fixed route and added testable content

--- app/controllers/call_controller.rb
+++ app/controllers/call_controller.rb
@@ -1,10 +1,13 @@
 class CallController < ApplicationController
-  def create
-    
-    phoneml = {:phoneml => [:speak => {"$" => params[:callerinput]}]}
-    
-    params
-    
+  def start
+
+    phoneml = {:phoneml => [:speak => {"$" => "Welcome weary traveler. You stand in front of a large opening in the side of a mountain.", "@voice" => "allison"}, 
+               :input => {"$" => "#{request.url}call/dungeon_master","@options" => "0,dungeon,enter,forward,run,flee"},]}
+
     render :json => phoneml.to_json
+  end
+
+  def dungeon_master
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ ActionController::Routing::Routes.draw do |map|
   # Sample of named route:
   #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
   # This route can be invoked with purchase_url(:id => product.id)
+  map.call 'call/:action', :controller => 'call'
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   map.resources :products
@@ -31,13 +32,13 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "call", :action => 'create'
+  map.root :controller => "call", :action => 'start'
 
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  # map.connect ':controller/:action/:id'
+  # map.connect ':controller/:action/:id.:format'
 end
